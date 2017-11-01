e26d7ecddfb19e2eba7a5d66195f6e23fb06cb28
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-01 19:56:57 +0000

    Divided the components of the index
    - Created Header component
    - Created Intro component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Intro />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+      </div>
+    );
+  }
+}
+
+class Intro extends Component {
+  render() {
+    return (
+      <div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
