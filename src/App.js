965b1b5e0e59894ab577a8eed767bef419f66535
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-01 20:24:19 +0000

    Divided components into separate files
    - Created a separate file for Header component
    - Created a separate file for Intro component
    - Moved all the code of those components from App to Header and Intro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Intro from './Intro';
 
 class App extends Component {
   render() {
@@ -8,31 +9,6 @@ class App extends Component {
       <div className="App">
         <Header />
         <Intro />
-      </div>
-    );
-  }
-}
-
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-      </div>
-    );
-  }
-}
-
-class Intro extends Component {
-  render() {
-    return (
-      <div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
